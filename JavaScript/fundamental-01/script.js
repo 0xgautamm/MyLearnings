7fda0650384ebeb86dcb0e46d47d3b020a5c97cb
0xgautamm <gautammmmsonii@gmail.com>
2022-09-29 11:42:21 +0530

    new properties learned of javascript

---
+++ JavaScript/fundamental-01/script.js
@@ -0,0 +1,121 @@
+// let js = "amazing";
+// // if (js === "amazing") alert("Javascript is alot of fun");
+
+// 23 + 7 - 10 + 20;
+// console.log(23 + 7 - 10 + 29);
+
+// let firstName = "oxETH";
+// console.log(firstName);
+
+// let first_person_name = "marvel's avengers";
+
+// console.log(first_person_name);
+
+// console.log(first_person_name);
+
+// console.log(first_person_name);
+
+// console.log(first_person_name);
+
+// let myFirstJob = "student towards school";
+// let myCurrentSituation = "boy towards family";
+
+// console.log(myCurrentSituation);
+// console.log(myFirstJob);
+
+// console.log(myFirstJob);
+// console.log(myCurrentSituation);
+
+// let javascriptIsFun = true;
+
+// console.log(javascriptIsFun);
+
+// console.log(typeof javascriptIsFun);
+
+// // console.log(typeof false);
+// // console.log(typeof 24);
+// // console.log(typeof "jonas schwadeft");
+
+// javascriptIsFun = "fun!";
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 2002;
+// console.log(typeof year);
+// console.log(typeof null);
+// MATHSoperator.....
+// const year = 2040;
+// const ageSach = year - 1992;
+// const ageDoms = year - 1990;
+// console.log(ageSach, ageDoms);
+
+// console.log(ageSach * 50, ageSach / 12, 5 ** 10);
+
+// const firstName = "Gaurav";
+// const finalName = "Taneja";
+
+// console.log(firstName + finalName);
+// //assignment operator
+// let x = 20 - 10 + 20 + 80 - 10;
+// x += 100;
+// x -= 120;
+// x++;
+// x--;
+// console.log(x);
+
+// // comparison operator
+// console.log(ageSach > ageDoms);
+// console.log(ageSach >= 48);
+// console.log(ageDoms <= 55);
+// console.log(ageDoms <= 45);
+
+// console.log(year - 1992 < year - 1990);
+
+// assignments
+// variable and values
+// let country = "India";
+// console.log(country);
+
+// let continent = "Asia";
+// console.log(continent);
+
+// let population = "32.00  Million";
+// console.log(population);
+// // DATA TYPE
+// let isIsland = true;
+// let language = "Hindi";
+// console.log(typeof isIsland);
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
+// // CONST/LET
+// const language = "Hindi";
+// console.log(language);
+// const country = "India";
+// console.log(country);
+// const continent = "Asia";
+// console.log(continent);
+// const population = 21;
+// console.log(population);
+// let isIsland = "true";
+//BASIC OPERATOR
+const populationIndia = 100;
+console.log(populationIndia / 2);
+const countryIndia = 100;
+console.log(countryIndia);
+let population = 100;
+console.log(population + 1);
+
+const finlandPopulation = "6";
+console.log(finlandPopulation);
+console.log(countryIndia > finlandPopulation);
+console.log(countryIndia <= 33);
+
+const morePopultion = countryIndia <= 33;
+
+const description =
+  "Portugal is in europe and it has 16 million people who spoke Portuguese";
+console.log(description);
