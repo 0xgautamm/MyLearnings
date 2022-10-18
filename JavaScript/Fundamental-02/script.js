22e90acb829951ce38a20c33572756b54bb81952
0xgautamm <gautammmmsonii@gmail.com>
2022-10-18 12:18:03 +0530

    learned strict mode

---
+++ JavaScript/Fundamental-02/script.js
@@ -0,0 +1,7 @@
+"strict code";
+
+let hasDriveLicense = true;
+const passTest = true;
+
+if (passTest) hasDriveLicense = true;
+if (hasDriveLicense) console.log("Anyone can drive vehicale after license :O ");
