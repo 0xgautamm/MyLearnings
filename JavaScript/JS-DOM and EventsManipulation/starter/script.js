e924096cfcece937fc35799a8f0719b5dcbc6b0f
0xgautamm <gautammmmsonii@gmail.com>
2023-01-24 18:52:41 +0530

    dom explained

--- JavaScript/JS-DOM and EventsManipulation/starter/script.js
+++ JavaScript/JS-DOM and EventsManipulation/starter/script.js
@@ -1,3 +1,6 @@
 'use strict';
 
-console.log( document.querySelector('.message'));
+console.log( document.querySelector('.message').textContent); 
+DOM(document object  model)= it helps to represent structured hmtl documents....... DOM allows javascript access to html elements and style css to manipulate..... 
+
+*querySelector* is the way for entry point in DOM js
