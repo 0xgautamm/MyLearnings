d3124ef37b27c72b0b46fa7bbd6c9a14dce7d196
0xgautamm <gautammmmsonii@gmail.com>
2023-04-11 00:16:35 +0530

    event learning

--- JavaScript/JS-DOM and EventsManipulation/starter/script.js
+++ JavaScript/JS-DOM and EventsManipulation/starter/script.js
@@ -1,18 +1,31 @@
 'use strict';
 
-console.log( document.querySelector('.message').textContent); 
-// DOM(document object  model)= it helps to represent structured hmtl documents....... DOM allows javascript access to html elements and style css to manipulate..... 
+// console.log( document.querySelector('.message').textContent); 
+// // DOM(document object  model)= it helps to represent structured hmtl documents....... DOM allows javascript access to html elements and style css to manipulate..... 
 
-// querySelector is the way for entry point in DOM js
+// // querySelector is the way for entry point in DOM js
 
- document.querySelector('.message').textContent = 'Yeah!!You Got The Answer🤾‍♂️🤾‍♀️' 
- document.querySelector('.message').textContent
+//  document.querySelector('.message').textContent = 'Yeah!!You Got The Answer🤾‍♂️🤾‍♀️' 
+//  document.querySelector('.message').textContent
 
-document.querySelector('.number').textContent= '9?'
-document.querySelector('.score').textContent='50'
+// document.querySelector('.number').textContent= '9?'
+// document.querySelector('.score').textContent='50'
 
-document.querySelector('.guess').value= 43
-console.log(document.querySelector('.guess').value)
+// document.querySelector('.guess').value= 43
+// console.log(document.querySelector('.guess').value)
 
-EventListerner =: any action done on the page is called an Event like moving mouse, clicking button, key press
-                      and EventListerner is reacting to certain events happened on screen 
+// EventListerner =: any action done on the page is called an Event like moving mouse, clicking button, key press
+//                       and EventListerner is reacting to certain events happened on screen 
+
+
+document.querySelector('.check').addEventListener( 'click', function(){
+ const guess=Number (document.querySelector('.guess').value)
+
+console.log(guess, typeof guess)
+
+
+if(!guess){
+  document.querySelector('.message').textContent='🚑No Number'
+}
+
+} );
