6a8d1c9c969d30a2d0b45a4982bcd10ce71cecfb
0xgautamm <gautammmmsonii@gmail.com>
2023-04-11 23:19:01 +0530

    learned math trunc

--- JavaScript/JS-DOM and EventsManipulation/starter/script.js
+++ JavaScript/JS-DOM and EventsManipulation/starter/script.js
@@ -1,9 +1,9 @@
-'use strict';
+// 'use strict';
 
-// console.log( document.querySelector('.message').textContent); 
-// // DOM(document object  model)= it helps to represent structured hmtl documents....... DOM allows javascript access to html elements and style css to manipulate..... 
+// // console.log( document.querySelector('.message').textContent); 
+// DOM(document object  model)= it helps to represent structured hmtl documents....... DOM allows javascript access to html elements and style css to manipulate..... 
 
-// // querySelector is the way for entry point in DOM js
+// querySelector is the way for entry point in DOM js
 
 //  document.querySelector('.message').textContent = 'Yeah!!You Got The Answer🤾‍♂️🤾‍♀️' 
 //  document.querySelector('.message').textContent
@@ -11,12 +11,16 @@
 // document.querySelector('.number').textContent= '9?'
 // document.querySelector('.score').textContent='50'
 
-// document.querySelector('.guess').value= 43
+// document.querySelector('.guess').value=11
 // console.log(document.querySelector('.guess').value)
 
 // EventListerner =: any action done on the page is called an Event like moving mouse, clicking button, key press
 //                       and EventListerner is reacting to certain events happened on screen 
 
+const number= Math.trunc(Math.random()*20)+1
+
+Math.trunc coded because of removing decimals given by *20.................
+*20 give its boundry number only to 19.9999999999 and just by adding 1 to code give its to 20th number too  
 
 document.querySelector('.check').addEventListener( 'click', function(){
  const guess=Number (document.querySelector('.guess').value)
@@ -27,5 +31,4 @@ console.log(guess, typeof guess)
 if(!guess){
   document.querySelector('.message').textContent='🚑No Number'
 }
-
-} );
+});
