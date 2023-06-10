4f485da3eacc977ea733cdd9b53df44c59662cf2
0xgautamm <gautammmmsonii@gmail.com>
2023-06-10 17:00:54 +0530

    manipulating

--- JavaScript/JS-DOM and EventsManipulation/starter/script.js
+++ JavaScript/JS-DOM and EventsManipulation/starter/script.js
@@ -19,7 +19,7 @@
 
 let secretNumber= Math.trunc(Math.random()*20)+1
 let score= 20;
-
+let highscore=0;
 // Math.trunc coded because of removing decimals given by *20.................
 //  20 give its boundry number only to 19.9999999999 and just by adding 1 to code give its to 20th number too  
 
@@ -40,28 +40,42 @@ else if (guess ===secretNumber){
   
   document.querySelector('.number').textContent= secretNumber;
 
-
   document.querySelector('.number').style.width = '35rem';
+  
+  if (score>highscore){
+     highscore=score;
+    document.querySelector('.highscore').textContent=highscore;
+  }
 
-
-// when the guess is too high
-
-}else if (guess > secretNumber){
- if(score>0){ document.querySelector('.message').textContent='AHH too high 🎃'
+  // when the guess is wrong
+} else if(guess !== secretNumber) {
+  if(score>0){
+     document.querySelector('.message').textContent=
+     guess > secretNumber?'AHH too high 🎃': 'FISSS too low👓'
   score--
   document.querySelector('.score').textContent=score;}
   
   else  document.querySelector('.message').textContent='You LoST the game BITCH'
-  } 
-// when the guess is too low
+}  
 
-else if (guess < secretNumber){
-if(score>0) { document.querySelector('.message').textContent='FISSS too low👓'
-score--
-  document.querySelector('.score').textContent=score;}
   
-  else  document.querySelector('.message').textContent='You LoST the game BITCH'
-}
+  // when the guess is too high
+// } else if (guess > secretNumber){
+//  if(score>0){ document.querySelector('.message').textContent='AHH too high 🎃'
+//   score--
+//   document.querySelector('.score').textContent=score;}
+  
+//   else  document.querySelector('.message').textContent='You LoST the game BITCH'
+//   } 
+// // when the guess is too low
+
+// else if (guess < secretNumber){
+// if(score>0) { document.querySelector('.message').textContent='FISSS too low👓'
+// score--
+//   document.querySelector('.score').textContent=score;}
+  
+//   else  document.querySelector('.message').textContent='You LoST the game BITCH'
+// }
 });
 
 // CODING CHALLENGE-01
@@ -72,11 +86,11 @@ document.querySelector('.again').addEventListener('click', function(){
 
 document.querySelector('.message').textContent='Start Guessing...';
 document.querySelector('.score').textContent=score;
+
 document.querySelector('.number').textContent='?';
 document.querySelector('.guess').value='';
 
 document.querySelector('body').style.backgroundColor =  '#212';
 document.querySelector('.number').style.width = '15rem';
 
-
-})
+});
