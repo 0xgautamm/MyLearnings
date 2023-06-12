cf8bb17d4d6dd020367156ae4bf77799ce24aba0
0xgautamm <gautammmmsonii@gmail.com>
2023-06-12 19:40:09 +0530

    modal script

---
+++ JavaScript/JS-DOM and EventsManipulation/Modal-Project/starter/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-Modal');
+
+const btnsShowModal = document.querySelectorAll('.show-Modal');
+console.log('btnsShowModal');
