021382b9aaf3433cdf076334bd8794f57ac1d0ad
0xgautamm <gautammmmsonii@gmail.com>
2023-06-13 16:17:05 +0530

    new tag

--- JavaScript/JS-DOM and EventsManipulation/Modal-Project/starter/script.js
+++ JavaScript/JS-DOM and EventsManipulation/Modal-Project/starter/script.js
@@ -2,7 +2,8 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-Modal');
+const btnCloseModal = document.querySelector('.close-modal');
 
-const btnsShowModal = document.querySelectorAll('.show-Modal');
+when the html has same 'class' for more than 1 element, we have to   use 'document.querySelectorAll'by which all elements having same class been selected by the selector
+const btnsShowModal = document.querySelectorAll('.show-modal');
 console.log('btnsShowModal');
