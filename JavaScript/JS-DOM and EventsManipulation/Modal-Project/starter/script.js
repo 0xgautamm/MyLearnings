ad9165b405d13fd54869f904178ba1981c84c830
0xgautamm <gautammmmsonii@gmail.com>
2023-06-14 16:26:25 +0530

    classList learned

--- JavaScript/JS-DOM and EventsManipulation/Modal-Project/starter/script.js
+++ JavaScript/JS-DOM and EventsManipulation/Modal-Project/starter/script.js
@@ -6,16 +6,16 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 // when the html has same 'class' for more than 1 element, we have to   use 'document.querySelectorAll'by which all elements having same class been selected by the selector
 const btnsShowModal = document.querySelectorAll('.show-modal');
-console.log('btnsShowModal');
+
+
+const showModal= function(){
+  console.log('Button clicked')
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
 
 for(let i=0; i < btnsShowModal.length; i++)
-btnsShowModal[i].addEventListener('click', function(){
-console.log('Button Clicked')
-
-modal.classList.remove('hidden');
-overlay.classList.remove('hidden');
-
-});
+btnsShowModal[i].addEventListener('click', showModal);
 
 const closeModal= function(){
   modal.classList.add('hidden');
@@ -23,17 +23,5 @@ const closeModal= function(){
 }
 
 
-btnCloseModal.addEventListener('click', function(){
-
-modal.classList.add('hidden');
-overlay.classList.add('hidden');
-})
-
-
-
-overlay.addEventListener('click', function(){
-
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-
-})
+btnCloseModal.addEventListener('click',closeModal)
+overlay.addEventListener('click',closeModal)
