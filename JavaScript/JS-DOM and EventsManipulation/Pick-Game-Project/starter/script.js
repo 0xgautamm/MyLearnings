a9c52fee7cd7e28c1dba0b8e42ed3ef8c2a4ea9b
0xgautamm <gautammmmsonii@gmail.com>
2023-06-19 20:14:21 +0530

    new project

---
+++ JavaScript/JS-DOM and EventsManipulation/Pick-Game-Project/starter/script.js
@@ -0,0 +1,6 @@
+const score0El = document.querySelector('#score--0');
+const  score01El = document.getElementById('score--1')
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent=0
+score01El.textContent=0
